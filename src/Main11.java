04e6e103a9b65dc9bcb23a19c9240ec8b33d2d7f
jungha.kim <jungha.kim@timf.co.kr>
2024-12-30 19:22:33 +0900

    11~12강 문자열

---
+++ src/Main11.java
@@ -0,0 +1,27 @@
+package src;
+
+import java.util.Scanner;
+
+/** 11강 문자열 압축 */
+public class Main11 {
+    public static void main(String[] args){
+        Main11 T = new Main11();
+        Scanner kb=new Scanner(System.in);
+        String str =kb.next();
+        System.out.println(T.solution(str));
+    }
+    public String solution(String str){
+        StringBuilder answer = new StringBuilder();
+        str =str+" "; //끝 지점 까지 확인을 위해 공백 추가
+        int cnt =1;
+        for(int i =0; i<str.length()-1; i++){
+            if(str.charAt(i) == str.charAt(i+1)) cnt++;
+            else{
+                answer.append(str.charAt(i));
+                if(cnt>1)answer.append(cnt);
+                cnt =1;
+            }
+        }
+        return answer.toString();
+    }
+}
---
+++ src/Main12.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.Scanner;
+
+/** 12강 암호*/
+public class Main12 {
+    public static void main(String[] args){
+        Main12 T = new Main12();
+        Scanner kb=new Scanner(System.in);
+        int cnt = kb.nextInt();
+        String str =kb.next();
+        System.out.println(T.solution(cnt,str));
+//        for (char x:T.solution(cnt,str)){
+//            System.out.printf(String.valueOf(x));
+//        }
+    }
+    public char[] solution(int cnt,String str){
+        String answer = str.replace("#","1");
+        answer = answer.replace("*","0");
+
+        int arrSize = str.length()/cnt;
+        int num = 0;
+        char[] answerArr = new char[cnt];
+        for(int i =0; i<answer.length(); i+=arrSize){
+            int end = arrSize * (num+1);
+            answerArr[num] = (char) Integer.parseInt(answer.substring(i,end), 2);
+            ++num;
+        }
+        return answerArr;
+    }
+    public String solution2(int cnt,String str){
+      StringBuilder answer = new StringBuilder();
+      int moq = str.length()/cnt;
+      for(int i =0; i<cnt; i++){
+          String tmp = str.substring(i,moq).replace("#","1").replace("*","0");
+          int num = Integer.parseInt(tmp,2); //2진수로 넘어 간다 -> 10진수로 변경
+          answer.append((char) num);
+      }
+      return answer.toString();
+    }
+}
