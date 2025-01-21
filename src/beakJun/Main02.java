553b45f3c4e15c7cbad2e7faf4d975c3a121b3a9
jungha.kim <jungha.kim@timf.co.kr>
2025-01-22 00:06:44 +0900

    설탕문제

---
+++ src/beakJun/Main02.java
@@ -0,0 +1,31 @@
+package src.beakJun;
+
+import java.util.Scanner;
+
+/** 설탕 배달 */
+public class Main02 {
+    public static void main(String[] args) {
+        Main02 T = new Main02();
+        Scanner kb = new Scanner(System.in);
+        int num = kb.nextInt();
+        System.out.println(T.solution(num));
+    }
+
+    public int solution(int num){
+        int result = 0;
+        while(num>0){
+            if(num>=5 && num%5==0){
+                result = result+num/5;
+                return result;
+            } else if(num<3){
+                result = -1;
+                return result;
+            }else{
+                result++;
+                num = num-3;
+            }
+        }
+
+        return result;
+    }
+}
