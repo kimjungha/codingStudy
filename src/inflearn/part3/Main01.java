d543e9f34dd0b0dce25964127c0ac5068e05f068
jungha.kim <jungha.kim@timf.co.kr>
2025-01-28 00:12:24 +0900

    투 포인터 알고리즘

--- src/inflearn/part3/Main01.java
+++ src/inflearn/part3/Main01.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 //강의 part 3 1번 문제 > 두 배열 합치기
+// 투 포인터 알고리즘으로 접근
 public class Main01 {
     public static void main(String[] args) {
         Main01 T = new Main01();
@@ -18,9 +19,33 @@ public class Main01 {
         for (int i =0; i<num2; i++){
             num2Array[i] = kb.nextInt();
         }
-        System.out.println(T.soultion(num1Array,num2Array));
+        for (int m:T.soultion(num1,num2,num1Array,num2Array)) {
+            System.out.print(m);
+        }
     }
 
+    public ArrayList<Integer> soultion(
+        int num1,int num2,int[] num1Array, int[]num2Array
+    ){
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0;
+        int p2 = 0;
 
+        while(p1<num1 && p2<num2){
+            if(num1Array[p1]<num2Array[p2]){
+                answer.add(num1Array[p1++]);
+            }else{
+                answer.add(num2Array[p2++]);
+            }
+        }
+        while(p1<num1){
+            answer.add(num1Array[p1++]);
+        }
+        while(p2<num2){
+            answer.add(num2Array[p2++]);
+        }
+
+        return answer;
+    }
 
 }
